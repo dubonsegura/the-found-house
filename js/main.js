f8b6ebeb17a24e3cc971370a6b0bf227063df682
dubonsegura <madubonsegura@hotmail.com>
2026-07-04 15:37:06 -0700

    Delete js directory

--- js/main.js
+++
@@ -1,3 +0,0 @@
-const header=document.querySelector('[data-header]');
-const button=document.querySelector('[data-menu-button]');
-if(button){button.addEventListener('click',()=>header.classList.toggle('open'));}
